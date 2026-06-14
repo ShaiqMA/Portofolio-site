7d65d40c2bbbd2f5c3653f9837439132ae3224ef
Shaiq.ahmed <shaiq.ahmed@student.rocva.nl>
2026-06-14 16:47:01 +0200

    sectie header en sectie project beschrijving volledig gestyled

--- resources/views/nieuw-project.blade.php
+++ resources/views/nieuw-project.blade.php
@@ -3,18 +3,20 @@
 @section('content')
 
 <!-- Header van de pagina  -->
-<div>
-    <h1>Schiphol-Dashboard</h1>
+<div class="bg-[#918574] justify-center p-6 flex items-center">
+    <div class="bg-[#B8AFA2] rounded-lg text-center inset-shadow-sm/40">
+        <h1 class="text-white px-10 py-6 text-[25px] font-extralight">Schiphol-Dashboard </h1>
+    </div>
 </div>
 
 <!-- Project beschrijving -->
-<div>
-    <div>
+<div class="bg-[#B8AFA2] flex flex-col justify-center items-center p-8 gap-6 text-white font-extralight">
+    <div class="bg-[#918574] px-4 py-8 rounded shadow-[0_0_20px_rgba(0,0,0,0.30)]">
         <p>
             Op luchthaven Schiphol vertrekken en landen dagelijks talloze vluchten. <br> Dit vereist een effectieve organisatie van vluchtgegevens, reizigersinformatie <br> en managementinzichten.
         </p>
     </div>
-    <div>
+    <div class="bg-[#918574] px-4 py-8 rounded shadow-[0_0_20px_rgba(0,0,0,0.30)]">
         <p>
             Dit project draait om het bouwen van een interactief <br>Schiphol-dashboard waarmee reizigers vluchten kunnen zoeken en boeken, <br>vluchtcoördinatoren vluchtschema’s en gatebeheer kunnen uitvoeren, en de directie overzichtelijke <br>rapportages ontvangt over omzet en gebruik.
         </p>
